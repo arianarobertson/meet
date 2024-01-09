192f10e0cadf91adfa88991bef8b4489b46f43d0
arianarobertson <arobertson22@berkeley.edu>
2024-01-08 17:56:37 -0800

    Added css styling

--- src/__test__/NumberOfEvents.test.js
+++ src/__test__/NumberOfEvents.test.js
@@ -1,6 +1,7 @@
 import userEvent from '@testing-library/user-event';
 import NumberOfEvents from '../components/NumberOfEvents';
-import { render } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
+import App from '../App'
 
 describe('<NumberOfEvents /> Component', () => {
     let NumberOfEventsComponent;
@@ -24,5 +25,23 @@ describe('<NumberOfEvents /> Component', () => {
         const input = NumberOfEventsComponent.queryByRole('textbox');
         await userEvent.type(input, '{backspace}{backspace}10');
         expect(input).toHaveValue('10');
+    });
+});
+
+//  integration testing
+describe('<NumberOfEvents /> integration', () => {
+    test('user can change the number of events displayed', async () => {
+        const user = userEvent.setup();
+        const AppComponent = render(<App />);
+        const AppDOM = AppComponent.container.firstChild;
+
+        const NumberOfEventsDOM = AppDOM.querySelector('#number-of-events');
+        const numberOfEventsInput = within(NumberOfEventsDOM).queryByRole('textbox');
+        await user.type(numberOfEventsInput, '{backspace}{backspace}10');
+
+        const EventListDOM = AppDOM.querySelector('#event-list');
+
+        const allRenderedEventItems = within(EventListDOM).queryAllByRole('listitem');
+        expect(allRenderedEventItems.length).toEqual(10);
     });
 });
